69dd91303c3c45d18ee8db2db769f4b4984344b5
Roman Geathers <romangeathersdev@gmail.com>
2022-06-12 13:38:15 -0400

    Refractor to improve efficiency
    Track iterations of array to avoid checking the final n elements
    of the array that coincide with the amount of iterations that have
    passed.

--- bubble.rb
+++ bubble.rb
@@ -1,12 +1,13 @@
 def bubble_sort(array)
     swap = true
+    iteration = 0
     while swap == true
         swap = false
         array.each_with_index do 
             |number, idx|
-            #add flag to check if any swaps were made, continue until no swaps. check one less
-            #idx for each iteration
             if array[idx + 1] == nil
+                next
+            elsif idx == array.length - 1 - iteration
                 next
             elsif array[idx] > array[idx + 1]
                 hold = array[idx + 1]
@@ -17,8 +18,10 @@ def bubble_sort(array)
                 next
             end
         end 
+        iteration += 1
     end
-    p array
+    array
 end
 
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([1,3,5,9,8,10])
+# => [0,2,2,3,4,78]
